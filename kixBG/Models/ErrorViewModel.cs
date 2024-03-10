e2c6e5d8ec3cc90e56f8b7acdbf6ee1339468b25
Kris <Kris@DESKTOP-P9TCQSQ>
2024-03-10 13:43:44 +0200

    Add project files.

---
+++ kixBG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kixBG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
