8e31178b1f662490e4e8c332bc02b87aaccc5417
grigorovv <krigorov2@gmail.com>
2024-03-25 16:30:54 +0200

    Made DataConstants changes.

--- kixBG.Infrastructure/Data/Constants/DataConstants.cs
+++ kixBG.Infrastructure/Data/Constants/DataConstants.cs
@@ -6,8 +6,10 @@
         
         public const int cityNameMaxLength = 20;
          
-        public const int clothingModelNameMaxLength = 45;
-         
+        public const int clothingModelNameMaxLength = 50;
+        public const int clothingModelNameMinLength = 8;
+        public const int clothesSizeMaxLength = 2;
+
         public const int countryNameMaxLength = 25;
         
         public const int sellerNameMaxLength = 40;
@@ -16,5 +18,8 @@
         public const int sellerPhoneNumberMinLength = 7;
 
         public const int shoeModelNameMaxLength = 40;
+
+        public const string minimumPrice = "0";
+        public const string maximumPrice = "100001";
     }
 }
