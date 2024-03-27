da5912607fdbc45a690f92470fac8a469bc41589
grigorovv <krigorov2@gmail.com>
2024-03-27 21:32:08 +0200

    Added custom 400, 401 and 404 Error pages and routing.

--- kixBG/Controllers/HomeController.cs
+++ kixBG/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using kixBG.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace kixBG.Controllers
 {
@@ -39,9 +37,24 @@ namespace kixBG.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+            else if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+            else if (statusCode == 404)
+            {
+                return View("Error404");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- kixBG/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace kixBG.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
